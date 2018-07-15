a87743a3dbee73bf13b33eaa2f92b58646245ba5
Dillon Coffman <dillonscoffman@gmail.com>
2018-07-15 12:50:25 -0400

    Added components, set up routes and cdn's

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <div class="nav-wrapper grey darken-3">
+            <div class="container">
+                <router-link to="/" class="brand-logo">Employee Manager</router-link>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'dashboard',
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
